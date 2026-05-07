3b5162f0b5c7079db545ddf669a01933e3eb725a
Giovanni d'Amelio <gio@damelio.net>
2026-05-07 11:19:27 -0700

    Make better completion keys

--- nvf/lsp.nix
+++ nvf/lsp.nix
@@ -49,7 +49,18 @@ in {
     autocomplete.blink-cmp = {
       enable = true;
       setupOpts = {
-        keymap.preset = "default";
+        keymap = {
+          preset = "none";
+          "<Tab>" = ["select_and_accept" "fallback"];
+          "<C-space>" = ["show" "show_documentation" "hide_documentation"];
+          "<C-e>" = ["hide"];
+          "<Up>" = ["select_prev" "fallback"];
+          "<Down>" = ["select_next" "fallback"];
+          "<C-p>" = ["select_prev" "fallback"];
+          "<C-n>" = ["select_next" "fallback"];
+          "<C-u>" = ["scroll_documentation_up" "fallback"];
+          "<C-d>" = ["scroll_documentation_down" "fallback"];
+        };
         appearance.nerd_font_variant = "mono";
         completion = {
           documentation.auto_show = false;
